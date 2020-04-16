a6fc524472a87d59099e20cc20a00b39318775fa
Ari Horowitz <ariehh@comcast.net>
2020-04-16 14:06:29 -0700

    layout of navtabs rendering on page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
-import Bootstrap from "./components/bootstrap";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+import Bootstrap from "./components/Bootstrap";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import TechnicalSkills from "./components/TechnicalSkills";
+import Resume from "./components/Resume";
+import Contact from "./components/Contact";
 
 function App() {
-  return <Bootstrap />;
+  return (
+    <Router>
+      <div>
+        <NavTabs />
+        {/* <Bootstrap /> */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/technicalskills" component={TechnicalSkills} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/resume" component={Resume} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react-portfolio/src/components/TechnicalSkills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TechnicalSkills() {
+  return <div>Technical Skills </div>;
+}
+
+export default TechnicalSkills;
