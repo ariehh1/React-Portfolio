91f2b0e27f8226bdc2c195b8ac8e1e5a681a8b8c
Ari Horowitz <ariehh@comcast.net>
2020-04-17 20:01:14 -0700

    components contain js and css files

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,45 +1,62 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/Home";
-// import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import TechnicalSkills from "./components/TechnicalSkills";
-import Resume from "./components/Resume";
-import Contact from "./components/Contact";
-// import About from "./components/About";
+import React, { Component } from "react";
+import InfoPanel from "./components/info-panel/InfoPanel";
+import DetailTabPanel from "./components/detail-tabpanel/DetailTabPanel";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      navroute: "",
-      totalroutes: [
-        "About",
-        "TechnicalSkills",
-        "Portfolio",
-        "Contact",
-        "Resume",
-      ],
-    };
-  }
+class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <NavTabs navroute={this.state.navroute} />
-          {/* <Bootstrap /> */}
-          <Switch>
-            <Route exact path="/" component={Home} />
-            {/* <Route exact path="/about" component={About} /> */}
-            <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/technicalskills" component={TechnicalSkills} />
-            <Route exact path="/contact" component={Contact} />
-            <Route exact path="/resume" component={Resume} />
-          </Switch>
-        </div>
-      </Router>
+      <div className="App">
+        <InfoPanel></InfoPanel>
+        <DetailTabPanel></DetailTabPanel>
+      </div>
     );
   }
 }
+
 export default App;
+
+// import React from "react";
+// import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+// import NavTabs from "./components/NavTabs";
+// import Home from "./components/Home";
+// // import About from "./components/About";
+// import Portfolio from "./components/Portfolio";
+// import TechnicalSkills from "./components/TechnicalSkills";
+// import Resume from "./components/Resume";
+// import Contact from "./components/Contact";
+// // import About from "./components/About";
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       navroute: "",
+//       totalroutes: [
+//         "About",
+//         "TechnicalSkills",
+//         "Portfolio",
+//         "Contact",
+//         "Resume",
+//       ],
+//     };
+//   }
+//   render() {
+//     return (
+//       <Router>
+//         <div>
+//           <NavTabs navroute={this.state.navroute} />
+//           {/* <Bootstrap /> */}
+//           <Switch>
+//             <Route exact path="/" component={Home} />
+//             {/* <Route exact path="/about" component={About} /> */}
+//             <Route exact path="/portfolio" component={Portfolio} />
+//             <Route exact path="/technicalskills" component={TechnicalSkills} />
+//             <Route exact path="/contact" component={Contact} />
+//             <Route exact path="/resume" component={Resume} />
+//           </Switch>
+//         </div>
+//       </Router>
+//     );
+//   }
+// }
+// export default App;
